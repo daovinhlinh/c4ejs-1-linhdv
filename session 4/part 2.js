258bbc7ee757dc177ab13ab73d571e9817c9adf2
Dao Vinh Linh <linh142000@gmail.com>
2019-10-14 22:15:19 +0700

    update part 2

--- session 4/part 2.js
+++ session 4/part 2.js
@@ -1,37 +1,116 @@
-// let word = ['to', 'be', 'that', 'of', 'elon', 'to', 'this', 'now', 'back', 'cool', 'hey', 'love', 'of', 'life', 'that', 'rain', 'summer', 'color', 'now', 'of', 'hat', 'late', 'sorry', 'my', 'team'];
-// let arrLength = word.length;
-// for (i = 0; i < arrLength; i++){
-//     for (j = 1; j < arrLength - 1; j++ ){
-//         if (word[i] === word[j])
-        
-//         console.log(`${word}: );
+let word = ['to', 'be', 'that', 'of', 'elon', 'to', 'this', 'now', 'back', 'cool', 'hey', 'love', 'of', 'life', 'that', 'rain', 'summer', 'color', 'now', 'of', 'hat', 'late', 'sorry', 'my', 'team'];
+let arrLength = word.length;
+word.sort();
+for (i = 0; i < arrLength; i++){
+    let count = 0;
+    for (j = 0; j <= arrLength ; j++ ){
+        if (word[i] === word[j]){
+        count++;
+        }
+    }
+    console.log(`${word[i]} : ${count}`);
+}
+
+
+//     for (i = 0; i <= arrLength ; i++ ){
+//         for (j = 0; j <= arrLength ; j++ ){
+//         if (word[i] != word[j])
+    
 //     }
-//     console.log(word);
 // }
 
+// const inventory = [
+//     {
+//         name: 'HP Envy 13 aq',
+//         price: 21000,
+//         brand: 'HP',
+//         quantity: 5,
+//     },
+//     {
+//         name: 'Dell XPS 9370',
+//         price: 30000,
+//         brand: 'Dell',
+//         quantity: 1,
+//     },
+//     {
+//         name: 'Dell Inspiron 3567',
+//         price: 9300,
+//         brand: 'Dell',
+//         quantity: 12,
+//     },
+//     {
+//         name: 'Dell Latitude E5450',
+//         price: 8600,
+//         brand: 'Dell',
+//         quantity: 2,
+//     },
+//     {
+//         name: 'Asus Zenbook',
+//         brand: 'Asus',
+//         price: 20000,
+//         quantity: 4,
+//     },
+//     {
+//         name: 'HP Pavilion',
+//         brand: 'HP',
+//         price: 14000,
+//         quantity: 7,
+//     },]
 
-
-
-
- // void Findrepeter(){
-    // let s="mmababctamantlslmag";
-    // let distinct = 0 ;
-
-    // for (let i = 0; i < s.length(); i++) {
-
-    //     for (let j = 0; j < s.length(); j++) {
-
-    //         if(s.charAt(i)==s.charAt(j))
-    //         {
-    //             distinct++;
-
-    //         }
-    //     }   
-    //     console.log(s.charAt(i)+"--"+distinct);
-    //     let d=String.valueOf(s.charAt(i)).trim();
-    //     s=s.replaceAll(d,"");
-    //     distinct = 0;
-
-    // }
-
+//     let inventoryByBrand = {
+//         dell: [
+//             {
+//                 name: 'Dell XPS 9370',
+//                 price: 30000,
+//                 brand: 'Dell',
+//                 quantity: 1,
+//             },
+//             {
+//                 name: 'Dell Inspiron 3567',
+//                 price: 9300,
+//                 brand: 'Dell',
+//                 quantity: 12,
+//             },
+//             {
+//                 name: 'Dell Latitude E5450',
+//                 price: 8600,
+//                 brand: 'Dell',
+//                 quantity: 2,
+//             },
+//         ],
+//         hp: [
+//                 {
+//                     name: 'HP Envy 13aq',
+//                     price: 21000,
+//                     brand: 'HP',
+//                     quantity: 5,
+//                 },
+//                 {
+//                     name: 'HP Pavilion',
+//                     brand: 'HP',
+//                     price: 14000,
+//                     quantity: 7,
+//                 },
+//             ],
+//         asus: [
+//            {
+//                name: 'Asus Zenbook',
+//                brand: 'Asus',
+//                price: 20000,
+//                quantity: 4,
+//            },
+//            ]
+//     }
+  
+ 
+ 
+// let count = prompt('Which brand?');
+// var arr = [];
+// var totalprice = 0;
+// for (let i = 0 ; i < inventoryByBrand[count].length ; i++){
+//     arr.push(inventoryByBrand[count][i].name);
+//     totalprice = totalprice + inventoryByBrand[count][i].price * inventoryByBrand[count][i].quantity * 1000 ;
 // }
+
+// alert(`There are ` +inventoryByBrand[count].length +count+ `laptop in inventory \n` +arr.join(`\n`) +`\nTotal price: `+totalprice);  
+
